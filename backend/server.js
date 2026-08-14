fc48b93f3b0a0a6b1cde37d07f980e753d29953f
amanrajput111 <abhayrajput112005@gmail.com>
2026-08-14 23:53:51 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,15 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser());
 
-  app.use(
-    cors("*")
-  );
+ app.use(
+  cors({
+    origin: [
+      "https://shopeease-2.onrender.com",
+      "https://shopease-3.onrender.com",
+    ],
+    credentials: true,
+  })
+);
   
 ConnectDB()
 
